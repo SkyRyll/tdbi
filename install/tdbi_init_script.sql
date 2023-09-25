887b25cf9d65ff1348fbb290dbbe800a8afd5779
skyryll <skyryll1987@gmail.com>
2023-09-25 10:44:19 +0200

    added more animals to catalog

--- install/tdbi_init_script.sql
+++ install/tdbi_init_script.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS tdbi;
-CREATE DATABASE tdbi;
+CREATE DATABASE IF NOT EXISTS tdbi;
 USE tdbi;
 
+DROP TABLE catalog;
 CREATE TABLE IF NOT EXISTS catalog(
   id int NOT NULL AUTO_INCREMENT,
   image varchar(255),
@@ -32,3 +32,31 @@ INSERT INTO catalog (image, scientificName, commonName, category, origin) VALUES
 "Mexican Red Knee",
 "New World Terrestrial",
 "Mexico");
+
+INSERT INTO catalog (image, scientificName, commonName, category, origin) VALUES (
+"https://spidersworld.eu/1371-large_default/cyriopagopus-sp-hati-hati-25cm.jpg",
+"Cyriopagopus sp. Hatihati",
+"Purple Earth Tiger",
+"Old World Arboreal",
+"Indonesia");
+
+INSERT INTO catalog (image, scientificName, commonName, category, origin) VALUES (
+"https://upload.wikimedia.org/wikipedia/commons/e/e9/Harpactira_pulchripes01.jpg",
+"Harpactira Pulchripes",
+"Golden blue-legged Baboon",
+"Old World Terrestrial",
+"South Africa");
+
+INSERT INTO catalog (image, scientificName, commonName, category, origin) VALUES (
+"https://www.exoticsunlimitedusa.com/cdn/shop/products/481f4e_2fbd9604310444ba95b11e71552a525f_mv2_60e43bef-ea8d-49bd-8561-ccc0907bfe95.jpg?v=1682317520",
+"Lasiocyano Sazimai",
+"Brazilian Blue",
+"New World Terrestrial",
+"Brazil");
+
+INSERT INTO catalog (image, scientificName, commonName, category, origin) VALUES (
+"https://argiopeterra.pl/userdata/public/gfx/893/ce7661d7bffe49b8a6b4dc81fde05f6e.jpg",
+"Chilobrachys Natanicharum",
+"Electric Blue",
+"Old World Terrestrial",
+"Thailand");
