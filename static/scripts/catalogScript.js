3200b06a2874b30966fdd06a6e030076485d43ea
Notmadeyet <Nickkipper@web.de>
2023-10-24 17:04:36 +0200

    Aliigned Text to centre

--- static/scripts/catalogScript.js
+++ static/scripts/catalogScript.js
@@ -12,13 +12,13 @@ $(document).ready(function () {
                         entry.scientificName +
                         '\')"><div class="row  m-1 bg-dark"><div class="col-md m-4 p-0  image-container"><img src="' +
                         entry.image +
-                        '" id="img"></div><div class="col-md">' +
+                        '" id="img"></div><div class="col-md txt-center">' +
                         entry.scientificName +
-                        '</div><div class="col-md">' +
+                        '</div><div class="col-md txt-center">' +
                         entry.commonName +
-                        '</div><div class="col-md">' +
+                        '</div><div class="col-md txt-center">' +
                         entry.category +
-                        '</div><div class="col-md">' +
+                        '</div><div class="col-md txt-center">' +
                         entry.origin +
                         "</div></div></li>"
                 );
